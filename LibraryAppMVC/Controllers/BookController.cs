bad53b6d6fdde4fc4835d75b8bac03486845cac5
Unknown <lizardswimmer@gmail.com>
2017-12-11 17:22:17 -0800

    init

---
+++ LibraryAppMVC/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using DatabaseConnect;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAppMVC.Controllers
+{
+    [Route("/other/")]
+    public class BookController : Controller
+    {
+        private Context _ctx;
+        public BookController(Context context)
+        {
+            _ctx = context;
+        }
+
+        [Route("thing")]
+        public IActionResult GetABook()
+        {
+            var a = _ctx.Books
+                    .Where(b => b.PageCount > 10)
+                    .Select(b => new { title = b.Title }) //Anonymous object
+                    .ToList();
+            return Json(a);
+        }
+    }
+}
---
+++ LibraryAppMVC/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DatabaseConnect;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAppMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(Configuration);
+            
+            var CnString = @"Server=tcp:lizardswimmer-dbserver.database.windows.net,1433;Initial Catalog=lizardswimmer-db;Persist Security Info=False;User ID=jamd315;Password=myDBPassword!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<Context>(options => options.UseSqlServer(CnString));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
