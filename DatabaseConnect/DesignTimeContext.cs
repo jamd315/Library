b2ec62f41300771e6b4531df47b1fb752f618c97
Unknown <lizardswimmer@gmail.com>
2018-04-19 00:33:22 -0700

    database pw changed in Azure, database backups added, project ended :(

--- DatabaseConnect/DesignTimeContext.cs
+++ DatabaseConnect/DesignTimeContext.cs
@@ -7,7 +7,7 @@ namespace DatabaseConnect
     {
         public Context CreateDbContext(string[] args)
         {
-            var CnString = @"Server=tcp:lizardswimmer-dbserver.database.windows.net,1433;Initial Catalog=lizardswimmer-db;Persist Security Info=False;User ID=jamd315;Password=ALongCreativePasswordForTheDatabase:);MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            var CnString = @"Server=tcp:lizardswimmer-dbserver.database.windows.net,1433;Initial Catalog=lizardswimmer-db;Persist Security Info=False;User ID=jamd315;Password={PW redacted, note to self, check PW document or reset in Azure portal};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             var CntxtBuilder = new DbContextOptionsBuilder<Context>()
                     .UseSqlServer(CnString)
                     .Options;
